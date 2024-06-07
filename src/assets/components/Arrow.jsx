647b6a5124c100869eb3fd941d3793ee11aeb93c
Tioluwani <codemeastro101@gmail.com>
2024-06-07 17:05:03 +0100

    worked on some little bugs

--- src/assets/components/Arrow.jsx
+++ src/assets/components/Arrow.jsx
@@ -4,13 +4,13 @@ import right_arrow_2_black from "/assets/images/arrow-2-right-long-black.svg";
 
 export default function Arrow({ text, link, color }) {
   let styles =
-    "w-fit flex gap-[18px] bg-white text-[#333333] py-[20px] pl-[20.34px] pe-[49.47px]";
+    "w-fit flex items-center gap-[18px] bg-white text-[#333333] py-[20px] pl-[20.34px] pe-[49.47px]";
 
   let type = false;
 
   if (color == "black") {
     styles =
-      "w-fit flex gap-[18px] bg-[#333333] text-white py-[20px] pl-[20.34px] pe-[49.47px]";
+      "w-fit flex items-center gap-[18px] bg-[#333333] text-white py-[20px] pl-[20.34px] pe-[49.47px]";
     type = true;
   }
 
